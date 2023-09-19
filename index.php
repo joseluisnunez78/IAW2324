81058b03900d41b4ac54b4e5636c2c6f89e32c5a
Jose Luis Núñez <joseluisnunez@iesamachado.org>
2023-09-19 19:16:34 +0200

    Añadido código php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mi primera web</title>
+    <title>Mi primera web!!!</title>
 </head>
 <body>
-    
+    <? echo "Hola mundo desde PHP!"; ?>
+    <? echo "Esto es una prueba"; ?>
 </body>
 </html>
